e32fcd981ccb9701f40e012087c84fb25b841b5c
Cameron Gray <cameron@camerongray.me>
2015-05-18 17:11:28 +0100

    Added module for module class

---
+++ client/module.py
@@ -0,0 +1,48 @@
+from enum import Enum
+from abc import ABCMeta, abstractmethod
+
+class Module(metaclass=ABCMeta):
+    @abstractmethod
+    def get_result(self):
+        pass
+
+
+class ResultLevel(Enum):
+    critical = 3
+    major = 2
+    minor = 1
+    ok = 0
+
+
+class ModuleResult(object):
+    def __init__(self, result_level, message="", value=0):
+        self._result_level = result_level
+        self._message = message
+        self._value = value
+
+    @property
+    def result_level(self):
+        return self._result_level
+    @result_level.setter
+    def result_level(self, value):
+        self._result_level = value
+
+    @property
+    def message(self):
+        return self._message
+    @message.setter
+    def message(self, value):
+        self._message = value
+    
+    @property
+    def value(self):
+        return self._value
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+    def __repr__(self):
+        return "<ModuleResult level: {0}, message: '{1}', value: {2}>".format(self.result_level, self.message, self.value)
+    
+    
+    
