929145ee955a8cf472824aec6530fecaf9d154e5
Cameron Gray <cameron@camerongray.me>
2015-06-10 21:55:37 +0000

    Added basic page for group management

--- web/web.py
+++ web/web.py
@@ -13,5 +13,9 @@ def index():
 def clients():
 	return render_template("clients.html")
 
+@app.route("/groups/")
+def groups():
+    return render_template("groups.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
