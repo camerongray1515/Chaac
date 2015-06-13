6c713ad175eb1ae5ad92fae039b4a5b4c20d1fd4
Cameron Gray <cameron@camerongray.me>
2015-06-13 21:15:20 +0000

    Plugins are now displayed in the "installed plugins" list

--- web/static/js/plugins.js
+++ web/static/js/plugins.js
@@ -1,1 +1,21 @@
-var plugins = {}
+var plugins = {
+    updatePluginList: function() {
+        $("#loading-message").removeClass("hidden");
+        $("#table-plugins").addClass("hidden");
+
+        data.getPlugins(function(response) {
+            $("#table-plugins > tbody").html("");
+
+            var html = ui.compileAndRenderTemplate("template-plugin", response);
+
+            $("#table-plugins > tbody").append(html);
+
+            $("#loading-message").addClass("hidden");
+            $("#table-plugins").removeClass("hidden");
+        });
+    }
+}
+
+$(document).ready(function() {
+    plugins.updatePluginList();
+});
