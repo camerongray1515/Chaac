33af72d08a5d9699792e2066d7782d8b802e47d8
Cameron Gray <cameron@camerongray.me>
2015-06-14 22:00:24 +0000

    Rendered modal for assigning plugins is now shown when the new "Manage Assignemnts" button is clicked

--- web/static/js/plugins.js
+++ web/static/js/plugins.js
@@ -13,9 +13,28 @@ var plugins = {
             $("#loading-message").addClass("hidden");
             $("#table-plugins").removeClass("hidden");
         });
+    },
+    assignPlugin: function() {
+        var pluginID = $(this).attr("data-plugin-id");
+
+        data.getPlugin(pluginID, function(response) {
+            // If the plugin does not exist, handle the error and then abort rendering the modal
+            if (!response.success) {
+                ui.showAlert("installed-plugins-alert-container", false, response["message"]);
+                return;
+            }
+
+            // Now render the template for the modal, put it on the page and then show it
+            var html = ui.compileAndRenderTemplate("template-modal-assign-plugin", response);
+            console.log(response);
+
+            $("#compiled-modal").html(html);
+            $("#compiled-modal > .modal").modal();
+        });
     }
 }
 
 $(document).ready(function() {
     plugins.updatePluginList();
+    $("#table-plugins").on("click", ".btn-assign-plugin", plugins.assignPlugin);
 });
