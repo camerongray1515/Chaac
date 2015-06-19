cec9e52e7efadc39d164d3dd2b0a20f75dcb5859
Cameron Gray <cameron@camerongray.me>
2015-06-19 21:36:04 +0000

    Added basic page for schedule stuff along with empty JS file and appropriate URL routes

--- web/web.py
+++ web/web.py
@@ -21,5 +21,9 @@ def groups():
 def plugins():
     return render_template("plugins.html")
 
+@app.route("/schedule/")
+def schedule():
+    return render_template("schedule.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
